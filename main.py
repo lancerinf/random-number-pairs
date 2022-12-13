06323986cf52db446de6e02dbe6283d1076851ca
Federico Lancerin <federico.lancerin@gmail.com>
2022-12-13 07:39:39 +0100

    names cleanup

--- main.py
+++ main.py
@@ -4,43 +4,39 @@ import random
 PHONE_NUMBER_LIST = {"Friend 0", "Friend 1", "Friend 2", "Friend 3", "Friend 4", "Friend 5"}
 
 
-def valid_pairs(pairs: list[tuple[str, str]]) -> bool:
+def valid_pairs(pairs_to_validate: list[tuple[str, str]]) -> bool:
     valid = True
 
-    if len(pairs) == 0:
+    if len(pairs_to_validate) == 0:
         return False
 
-    for pair in pairs:
-        if pair[0] == pair[1]:
+    for pair_to_validate in pairs_to_validate:
+        if pair_to_validate[0] == pair_to_validate[1]:
             valid = False
     return valid
 
 
 def create_pairs() -> list[tuple[str, str]]:
+    new_pairs: list[tuple[str, str]] = []
     used_numbers = set()
-    number_pairs: list[tuple[str, str]] = []
 
     for entry in PHONE_NUMBER_LIST:
         possible_pairs = PHONE_NUMBER_LIST.copy()
-        possible_list = []
-
         possible_pairs.remove(entry)
         possible_pairs.difference_update(used_numbers)
 
         if len(possible_pairs) == 0:
-            # add invalid entry to trigger a re-do
-            list.append(number_pairs, (entry, entry))
-            return number_pairs
+            # return invalid list of pairs to trigger a re-pair. This might happen in the corner case where the one
+            # remaining entry to pair hasn't been matched before and only has itself to match with.
+            return []
 
-        for num in possible_pairs:
-            list.append(possible_list, num)
+        possible_matches = list(possible_pairs)
+        selected_match = possible_matches[random.randint(0, len(possible_matches) -1)]
 
-        selected_num = possible_list[random.randint(0, len(possible_list) -1)]
+        used_numbers.add(selected_match)
+        list.append(new_pairs, (entry, selected_match))
 
-        used_numbers.add(selected_num)
-        list.append(number_pairs, (entry, selected_num))
-
-    return number_pairs
+    return new_pairs
 
 
 if __name__ == '__main__':
